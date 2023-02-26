8fab792e830973df374f8eb108749dec868e1c13
DilipKumarSingh <dilipkr.iitkgp@gmail.com>
2023-02-26 10:18:30 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/:name", IndexHandler)
+	router.Run()
+}
+
+func IndexHandler(c *gin.Context) {
+	name := c.Params.ByName("name")
+	c.JSON(200, gin.H{
+		"message": "hello " + name,
+	})
+}
