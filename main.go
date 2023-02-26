1725bbde8e0cf6ab81bfa6be0ff78eef300a403b
DilipKumarSingh <dilipkr.iitkgp@gmail.com>
2023-02-26 11:56:37 +0530

    adding XML as well as Json Response for the  API endpoint

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/xml"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 	router.GET("/:name", IndexHandler)
+	router.GET("/", IndexHandlerXml)
 	router.Run()
 }
 
@@ -15,4 +18,17 @@ func IndexHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "hello " + name,
 	})
+}
+
+func IndexHandlerXml(c *gin.Context) {
+	c.XML(200, Person{
+		FirstName: "Dilip",
+		LastName:  "Singh",
+	})
+}
+
+type Person struct {
+	XMLName   xml.Name `xml:"person"`
+	FirstName string   `xml:"firstName,attr"`
+	LastName  string   `xml:"lastName,attr"`
 }
